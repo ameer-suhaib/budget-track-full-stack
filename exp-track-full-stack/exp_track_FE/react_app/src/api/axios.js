b2f74bd409d69ba55acc1dace0be0d3c77499c93
Ameer suhaib <ameercharath77@gmail.com>
2025-06-09 20:12:29 +0530

    endpoint modification

--- exp-track-full-stack/exp_track_FE/react_app/src/api/axios.js
+++ exp-track-full-stack/exp_track_FE/react_app/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://budget-track-full-stack.onrender.com',
+  baseURL: 'https://budget-track-full-stack.onrender.com/api',
 });
 
 API.interceptors.request.use((req) => {
