0bc895be1c0bbb26b2fc029a15059f424b62c5bd
Ameer suhaib <ameercharath77@gmail.com>
2025-06-09 20:06:21 +0530

    auth

--- exp-track-full-stack/exp_track_FE/react_app/src/api/axios.js
+++ exp-track-full-stack/exp_track_FE/react_app/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://budget-track-full-stack.onrender.com',
 });
 
 API.interceptors.request.use((req) => {
